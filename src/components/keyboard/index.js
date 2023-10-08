055522497f7870bf27573831ab20af788f407c25
yash <yashwanthvs@gmail.com>
2023-10-09 03:28:39 +0530

    add clicking on keyboard tiles to enter letter

--- src/components/keyboard/index.js
+++ src/components/keyboard/index.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Box, Typography } from "@mui/material";
 import { colors } from "../../constants";
+import BackSvg from "../../assets/backSvg";
 
 const Keyboard = () => {
   const keyList = "QWERTYUIOPASDFGHJKLeZXCVBNMb";
   const keys = [];
+  const handleClick = (key) => {
+    let keyStroke;
+    switch (key) {
+      case "e":
+        keyStroke = "Enter";
+        break;
+      case "b":
+        keyStroke = "Backspace";
+        break;
+      default:
+        keyStroke = key;
+    }
+    document.dispatchEvent(new KeyboardEvent("keyup", { key: keyStroke }));
+  };
   for (let i = 0; i < keyList.length; i++) {
     const key = keyList[i];
 
@@ -14,7 +29,7 @@ const Keyboard = () => {
     let width;
 
     if (key === "e" || key === "b") {
-      out = key === "e" ? "ENTER" : "BACK";
+      out = key === "e" ? "Enter" : "Backspace";
       variant = "caption";
       spacing = 3;
       width = "60px";
@@ -26,19 +41,26 @@ const Keyboard = () => {
     }
 
     keys.push(
-      <Grid item sm={spacing} sx={{ padding: 0 }}>
+      <Grid key={i} item sm={spacing} sx={{ padding: 0 }}>
         <Box
           style={{
             height: "55px",
             width: width,
             display: "flex",
             background: colors.keyUnused,
-            borderRadius: '4px'
+            borderRadius: "4px",
           }}
           justifyContent="center"
           alignItems="center"
+          onClick={() => handleClick(key)}
         >
-          <Typography variant={variant} sx={{fontWeight: 600}}>{out}</Typography>
+          {out === "Backspace" ? (
+            <BackSvg />
+          ) : (
+            <Typography variant={variant} sx={{ fontWeight: 600 }}>
+              {out}
+            </Typography>
+          )}
         </Box>
       </Grid>
     );
@@ -51,7 +73,7 @@ const Keyboard = () => {
       columns={20}
       justifyContent="center"
       alignItems={"center"}
-      sx={{padding: "20px", maxWidth: "500px" }}
+      sx={{ padding: "20px", maxWidth: "500px" }}
     >
       {keys}
     </Grid>
